31593d8a315d181f8992ef447cc2fa68aa2204a7
julien44830 <deniaudjulien0@gmail.com>
2024-11-12 11:05:35 +0100

    update app.js for testing new workflows github actions

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src={logo}
+                    className="App-logo"
+                    alt="logo"
+                />
+                <p>petit test</p>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
